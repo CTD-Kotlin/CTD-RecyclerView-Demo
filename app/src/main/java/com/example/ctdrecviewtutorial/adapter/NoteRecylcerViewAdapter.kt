a2f90b70306ad538e6e31d36c21089478485a53c
sortagreg <ladd.marshall@gmail.com>
2019-08-05 06:55:55 -0500

    create adapter

--- app/src/main/java/com/example/ctdrecviewtutorial/adapter/NoteRecylcerViewAdapter.kt
+++ app/src/main/java/com/example/ctdrecviewtutorial/adapter/NoteRecylcerViewAdapter.kt
@@ -1,5 +1,32 @@
 package com.example.ctdrecviewtutorial.adapter
 
-class NoteRecylcerViewAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ctdrecviewtutorial.models.Note
+
+class NoteRecylcerViewAdapter : ListAdapter<Note, NoteViewHolder>(NoteDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+    }
+
+}
+
+class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+}
+
+class NoteDiffCallback : DiffUtil.ItemCallback<Note>() {
+
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+    }
 
 }
